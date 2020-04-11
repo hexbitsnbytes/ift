454c66f79c34224a96d8f12dea84d534ed011297
Louis-Nicolas Gauthier <louis-nicolas.gauthier@openfleet.com>
2020-04-11 14:53:35 -0400

    plays with arrays

--- devoir4/shell-2.bash
+++ devoir4/shell-2.bash
@@ -4,12 +4,14 @@
 for line in $(cat $(pwd)/samples/liste.csv | sed '1d' | tr -d " ");
 do
     prefix=`echo $line | cut -d"," -f1,2`
-    results=`echo $line | cut -d"," -f 3,4,5,6,7`
+    results=(`echo $line | cut -d"," -f 3,4,5,6,7`)
     i=0
     # iterates over results for each student
     for (( i=1; i<=5; i++ ))
     do
         echo -n "${prefix},"
         echo $results | cut -d"," -f $i
-    
+        
+    done
+    echo "Results: ${results[0]} + ${results[1]}"
 done
