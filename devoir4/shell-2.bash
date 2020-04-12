1f00fcb8de3cf961f58b144298e1c3edcef1f2df
Louis-Nicolas Gauthier <louis-nicolas.gauthier@openfleet.com>
2020-04-12 10:25:41 -0400

    adds comments and removes unused lines

--- devoir4/shell-2.bash
+++ devoir4/shell-2.bash
@@ -5,14 +5,23 @@ command=$(cat $(pwd)/samples/liste.csv \
           | sed '1d' \
           | tr -d " ")
 
-# Returns all the lines from csv source file as elements of listByLine
+# Returns all the columns from source file to reuse in output file
+# ACKA16066604,AckouyetWilliam
+# ADOA10329606,AdouaAparihouraAngeMack
+# AGHS09118409,GauthieEddine
+# AITZ30535409,AitMarco
 studentPrefix() {
     for line in $command; do 
         echo "${line}" \
         | cut -f 1,2 -d","
     done
 }
+
 # Returns all the lines from csv source file as elements of listByLine
+#ACKA16066604,AckouyetWilliam,45.5,75,100,95,48.5,90
+#ADOA10329606,AdouaAparihouraAngeMack,83,64.75,95,87.5,74,97
+#AGHS09118409,GauthieEddine,83,55,95,87.5,41.5,97
+#AITZ30535409,AitMarco,69,72,80,67,70,100
 parsedList() {
     listByLine=()
 
@@ -25,7 +34,9 @@ parsedList() {
     echo ${listByLine}
 }
 
-# Returns a global variable with the list of averages
+# Returns a AVLIST global variable with the list of averages
+# echo $AVLIST     
+# 78.875  82.562  80.125  72.000
 AVLIST=()
 average() {
     # call elements by index: echo ${list[1]}
@@ -42,28 +53,26 @@ average() {
     
     for i in $(seq 1 $loop); do
         for line in $(echo ${list[@]} | cut -f $i -d" "); do
-            f0=$(echo $line | cut -f 3 -d",")
-            f1=$(echo $line | cut -f 4 -d",")
-            f2=$(echo $line | cut -f 5 -d",")
-            f3=$(echo $line | cut -f 6 -d",")
-            op1=$(echo $f0 + $f1 + $f2 + $f3 | bc )
-            op2=$(echo 4.0 | bc)
-            # This does not return the right value yet
-            quotient=$(echo "scale=3; ${op1} / ${op2}" | bc)
-            remainder=$(echo "scale=3; ${op1} % ${op2}" | bc)
+            # Assigns the results to variables
+            local f0=$(echo $line | cut -f 3 -d",")
+            local f1=$(echo $line | cut -f 4 -d",")
+            local f2=$(echo $line | cut -f 5 -d",")
+            local f3=$(echo $line | cut -f 6 -d",")
+            local op1=$(echo $f0 + $f1 + $f2 + $f3 | bc )
+            local op2=$(echo 4.0 | bc)
+            # Computes the averages
+            local quotient=$(echo "scale=3; ${op1} / ${op2}" | bc)
+            local remainder=$(echo "scale=3; ${op1} % ${op2}" | bc)
             counter=$(( ${counter} + 1))
-            #echo student $i
-            #echo -n "${quotient} "
             AVLIST+="$(echo -n "${quotient} ")"
-            #echo remainder: $remainder
         done
     done
 }
 average
 
-
+# Routine to ouput the studentPrefix with the computed average
 i=1
 for student in $(studentPrefix); do
-    echo ${student}, $(echo $AVLIST[${i}] | cut -f ${i} -d" ")
+    echo "${student},$(echo $AVLIST[${i}] | cut -f ${i} -d" ")"
     i=$(( ${i} + 1 ))
 done
