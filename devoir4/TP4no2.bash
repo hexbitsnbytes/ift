6a1328b88dab39a8d17f9c5200e8607bba65d825
PepeMaitreDesCodes <pierre-paul.hamon@bell.net>
2020-04-28 15:18:14 -0400

    Update TP4no2.bash

--- devoir4/TP4no2.bash
+++ devoir4/TP4no2.bash
@@ -2,35 +2,24 @@
 #TP4no2.bash script
 ligne="TP1(80),Intra(80),TP2(80),TP3(80),Final(40),TP4(80)"
 chaine=$ligne
-TP1=50
-Intra=50
-TP2=50
-TP3=50
-Final=51
-TP4=50
+declare -a notes  #0:TP1, 1:Intra, 2:TP2, 3:TP3, 4:Final, 5:TP4
 Total=0
 function calculTotal
 {
-	Total=`echo "scale=4; $Total+($TP1+$TP2+$TP3+$TP4)*7.5"|bc`
-	Total=`echo "scale=4; $Total+$Intra*30"|bc`
-	Total=`echo "scale=4; $Total+$Final*40"|bc`
+	TP=`echo "scale=4; ${notes[0]}+${notes[2]}+${notes[3]}+${notes[5]}"|bc`
+	Total=`echo "scale=4; $Total+$TP*7.5"|bc`
+	Total=`echo "scale=4; $Total+${notes[1]}*30"|bc`
+	Total=`echo "scale=4; $Total+${notes[4]}*40"|bc`
 	Total=`echo "scale=4; $Total/100"|bc`
 	echo $Total
 }
 function extraction
 {
-	chaine=${chaine#*(}
-	TP1=${chaine%%)*}
-	chaine=${chaine#*(}
-	Intra=${chaine%%)*}
-	chaine=${chaine#*(}
-	TP2=${chaine%%)*}
-	chaine=${chaine#*(}
-	TP3=${chaine%%)*}
-	chaine=${chaine#*(}
-	Final=${chaine%%)*}
-	chaine=${chaine#*(}
-	TP4=${chaine%%)*}
+	for i in 0 1 2 3 4 5 #0:TP1, 1:Intra, 2:TP2, 3:TP3, 4:Final, 5:TP4
+	do
+		chaine=${chaine#*(}
+		notes[$i]=${chaine%%)*}
+	done
 }
 extraction
 calculTotal
