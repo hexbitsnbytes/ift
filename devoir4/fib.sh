64a57a5d39cb382a7ca43dd14e5081acc9e065de
Louis-Nicolas Gauthier <louis-nicolas.gauthier@openfleet.com>
2020-04-29 13:35:33 -0400

    renames files, and cleans up output for fib.sh

--- devoir4/fib.sh
+++ devoir4/fib.sh
@@ -9,20 +9,19 @@ fib_array=(1)
 # Implements fibonnaci iterative algorithm
 # Returns an element of fibonnaci based on index
 fibonnaci() {
-    for (( i=0; i<10; i++ )); do
+    for (( i=0; i<1000; i++ )); do
 	    fib=$(( ${f0} + ${f1}))
 	    f0=$f1
 	    f1=$fib
-	    echo "Value of \$i is: "${i}" and fib is "${fib}""
+	    #echo "Value of \$i is: "${i}" and fib is "${fib}""
             fib_array+=" ${fib}"
     done
 }
-fibonnaci
-echo "Content of mem: "${fib_array}""
-# Cut is zero-indexed so add one to the field to cut
-select_indice="$(\
-    echo "${fib_array}" | \
-    cut -d" " -f $((${1} + 1)) \
-    )"
 
-echo "selected element of fibonnaci_array: "${select_indice}""
+if [ $# -eq 0 ]
+then
+    echo "No argument provided. Fist element in fibonacci is 1" 
+else 
+    fibonnaci
+    echo "${fib_array}" | cut -d" " -f $((${1} + 1))
+fi
