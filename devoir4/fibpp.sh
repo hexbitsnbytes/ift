9cf7dfe4f159dad6541c8ab5e6164f36256da568
PepeMaitreDesCodes <pierre-paul.hamon@bell.net>
2020-04-29 21:15:16 -0400

    Commentaire

--- devoir4/fibpp.sh
+++ devoir4/fibpp.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 #fibpp.sh script
+#Ce script en Bashe Shell sert à calculer un élément n de la suite Fibonacci
+#L'élément n est mis en paramètre après sh fib.sh
 n=$1
-nm2=1
-nm1=1
-f=1
-i=2
-while [ $i -le $n ]; do
-	f=$(($nm2+$nm1))
-	nm2=$nm1
+nm2=1	#n moins 2
+nm1=1	#n moins 1
+f=1		#somme des éléments de la suite de Fibonacci jusqu'à n
+i=2		#variable de loop
+while [ $i -le $n ]; do	#loop qui sert à calculer f et qui
+	f=$(($nm2+$nm1))	#arrète quand la variable de
+	nm2=$nm1			#loop i est plus grande que n
 	nm1=$f
-	i=$(($i+1))
+	i=$(($i+1))			#i augmente de 1 à chaque loop
 done
-echo $f
+echo "L'élément $n de la suite Fibonacci a pour valeur $f"
--- devoir4/moypp.sh
+++ devoir4/moypp.sh
@@ -1,46 +1,54 @@
 #!/bin/sh
 # moypp.sh script
-declare -a notes  #0:TP1, 1:Intra, 2:TP2, 3:TP3, 4:Final, 5:TP4
-function lireNotes
+#Ce script sert à lire des notes d'étudiants à partir d'un fichier
+#liste.csv pour ensuite calculer et écrire les notes finales
+#de ces étudiants dans un fichier moy.sh
+#Le fichier liste.csv devra être mis en paramètre après sh moy.sh
+
+declare -a notes	#tableau qui contient toutes les notes non-finales des étudiants  
+					#0:TP1, 1:Intra, 2:TP2, 3:TP3, 4:Final, 5:TP4
+					
+function lireNotes	#fonction qui sert à lire les notes à partir de liste.csv
 {
 	Total="Total"
-	>notes.csv
+	>notes.csv	#initialise le fichier notes.csv
 	while read ligne
 	do
 		ligne=${ligne%?}
 		chaine=$ligne
 		extraction
 		ecrireResultat
-		Total=0
+		Total=0			#réinitialise le total pour une nouvelle ligne
 	done < liste.csv
 }
-function extraction
+function extraction		#sert à extraire les notes des lignes lues
 {
-	if [ $Total != 0 ]
+	if [ $Total != 0 ]	#met fin à la fonction quand le total n'a pas été réinitialisé
 		then
 		return 0
 	fi
 	chaine=${chaine#*,}
 	for i in 0 1 2 3 4 5 #0:TP1, 1:Intra, 2:TP2, 3:TP3, 4:Final, 5:TP4
 	do
-		chaine=${chaine#*,}
-		notes[$i]=${chaine%%,*}
+		chaine=${chaine#*,}		#sépare les notes sur la ligne lue afin
+		notes[$i]=${chaine%%,*}	#de les mettre dans le tableau notes
 	done
 	calculTotal
 }
-function calculTotal
+function calculTotal	#sert à calculer le total à partir de toutes les notes
 {
 	TP=`echo "scale=4; ${notes[0]}+${notes[2]}+${notes[3]}+${notes[5]}"|bc`
 	Total=`echo "scale=4; $Total+$TP*7.5"|bc`
 	Total=`echo "scale=4; $Total+${notes[1]}*30"|bc`
 	Total=`echo "scale=4; $Total+${notes[4]}*40"|bc`
 	Total=`echo "scale=4; $Total/100"|bc`
-	Total=`printf "%g" ${Total/./,}`
+	Total=`printf "%g" ${Total/./,}`	#change le format de Total afin de suprimer
+										#tous les zéros inutiles
 	Total=${Total/,/.}
 }
-function ecrireResultat
+function ecrireResultat	#sert à écrire les notes finales dans notes.csv
 {
-	chaine=`echo $ligne,$Total`
-	echo $chaine >> notes.csv
+	chaine=`echo $ligne,$Total`	#reprend la ligne lue et ajoute le total à la fin
+	echo $chaine >> notes.csv	#écrit les notes dans notes.csv
 }
 lireNotes
